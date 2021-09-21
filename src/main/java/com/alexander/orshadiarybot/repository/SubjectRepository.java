7b427513921d699db54d08b22c0f6432a65a5622
Vorobeyyyyyy <mrskilkchat@gmail.com>
2021-09-21 22:33:14 +0300

    fix/remove dublicates in subjects list

--- src/main/java/com/alexander/orshadiarybot/repository/SubjectRepository.java
+++ src/main/java/com/alexander/orshadiarybot/repository/SubjectRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Repository
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
 
-    @Query(value = "SELECT s from Subject s join Mark m on m.subject.id=s.id WHERE m.account.id= ?1")
+    @Query(value = "SELECT s from Subject s join Mark m on m.subject=s WHERE m.account.id= ?1 GROUP BY s")
     List<Subject> findAllByAccountId(long accountId);
 
     Subject findById(long subjectId);

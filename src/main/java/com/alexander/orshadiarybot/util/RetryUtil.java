4a8d3cdca764b9266c8178d6322fc01368390708
Vorobeyyyyyy <gf6t5dSKILK>
2022-03-19 22:59:09 +0300

    feature: add sleep between retries, add logs

--- src/main/java/com/alexander/orshadiarybot/util/RetryUtil.java
+++ src/main/java/com/alexander/orshadiarybot/util/RetryUtil.java
@@ -1,21 +1,33 @@
 package com.alexander.orshadiarybot.util;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.function.Supplier;
 
+@Slf4j
 public class RetryUtil {
 
-    public static <T> T tryAndGetOrElseThrow(int repeatCount, long delay, Supplier<T> supplier, Supplier<RuntimeException> exceptionSupplier) {
-        StackTraceElement[] stackTrace = new StackTraceElement[0];
-        while (repeatCount > 0) {
+    public static <T> T tryAndGetOrElseThrow(int retryCount, long delay, Supplier<T> supplier, Supplier<RuntimeException> exceptionSupplier) {
+        if (retryCount <= 0) {
+            throw new IllegalArgumentException("retryCount must be greater than 0");
+        }
+        Exception lastException = null;
+        while (retryCount > 0) {
             try {
                 return supplier.get();
             } catch (Exception e) {
-                repeatCount--;
-                stackTrace = e.getStackTrace();
+                retryCount--;
+                log.info("Exception occurred while trying to get data from the source. {} attempts left", retryCount);
+                lastException = e;
+            }
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                log.warn("Interrupted while waiting for retry: {}", e.getMessage());
             }
         }
         RuntimeException runtimeException = exceptionSupplier.get();
-        runtimeException.setStackTrace(stackTrace);
+        runtimeException.addSuppressed(lastException);
         throw runtimeException;
     }
 }
